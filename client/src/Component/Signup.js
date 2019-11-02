a6ee9f764ef66f242a8a8e0c45f35707f2ce8430
Polinet <uchpolinet@gmail.com>
2019-11-03 00:36:18 +1100

    added an onclick listener to register button

--- client/src/Component/Signup.js
+++ client/src/Component/Signup.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import axios from 'axios';
 
 class Signup extends Component {
+
     constructor() {
         super()
         this.state = {
@@ -12,11 +13,13 @@ class Signup extends Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
     };
+
     handleChange(event) {
         this.setState({
             [event.target.name]: event.target.value
         });
     };
+
     handleSubmit(event) {
         console.log("sign-up handleSubmit, username: ");
         console.log(this.state.username);
@@ -25,6 +28,7 @@ class Signup extends Component {
     
     render() {
         return (
+            
         <div className="SignupForm">
             <h4>Register Now</h4>
             <form className="form-horizontal">
@@ -46,6 +50,23 @@ class Signup extends Component {
                     <div className="col-1 col-ml-auto">
                         <label className="form-label" htmlFor="password">Password: </label>
                     </div>
+                    <div className="col-3 col-mr-auto">
+                        <input className="form-input"
+                            placeholder="password" 
+                            type="password"
+                            name="password"
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                            />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="col-7"></div>
+                    <button className="btn btn-primary col-1 col-mr-auto"
+                        onClick={this.handleSubmit}
+                        type="submit">
+                        Register
+                    </button>
                 </div>
             </form>
         </div>
