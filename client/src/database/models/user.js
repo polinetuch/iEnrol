b075b8a0cb9145f0c8c784f1e9bd98028f8235c7
Polinet <uchpolinet@gmail.com>
2019-11-03 01:41:02 +1100

    define schema method to hash password

--- client/src/database/models/user.js
+++ client/src/database/models/user.js
@@ -16,3 +16,14 @@ const userSchema = new Schema({
         required: false
     }
 });
+
+// Define schema methods
+userSchema.methods = {
+    checkPassword: function (inputPassword) {
+        return bcrypt.compareSync(inputPassword, this.password)
+    },
+    hashPassword: plainTextPassword => {
+        return bcrypt.hashSync(plainTextPassword, 10)
+    }
+};
+
