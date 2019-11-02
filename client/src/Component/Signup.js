c53a93d9e84e636f5591dd9d34a6efdc9fa66a17
Polinet <uchpolinet@gmail.com>
2019-11-02 23:36:29 +1100

    added form group to render in signup component and import in app.js

--- client/src/Component/Signup.js
+++ client/src/Component/Signup.js
@@ -11,5 +11,39 @@ class Signup extends Component {
         }
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
+    };
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+    handleSubmit(event) {
+        console.log("sign-up handleSubmit, username: ");
+        console.log(this.state.username);
+        event.preventDefault();
     }
+}
+
+render() {
+    return (
+        <div className="SignupForm">
+            <h4>Register Now</h4>
+            <form className="form-horizontal">
+                <div className="form-group">
+                    <div className="col1 col-ml-auto">
+                        <label className="form-label" htmlFor="username">Username</label>
+                    </div>
+                    <div className="col-3 col-mr-auto">
+                        <input className="form-input" 
+                            type="text" 
+                            id="username" 
+                            placeholder="username" 
+                            value={this.state.username} 
+                            onChange={this.handleChange} 
+                        />
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
 }
