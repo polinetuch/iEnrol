9e8c7f1224ba6187a8ea3213c9641d2f1f040422
Polinet <uchpolinet@gmail.com>
2019-11-03 15:46:17 +1100

    added a condition to render navbar buttons

--- client/src/Component/navbar.js
+++ client/src/Component/navbar.js
@@ -25,7 +25,46 @@ class Navbar extends component {
                 })
             }
         }).catch(error => {
-            console.log("Logout error occurr");
+            console.log("Logout error occurr" + error);
         })
     }
+
+    render() {
+        const loggedIn = this.props.loggedIn;
+        console.log("navbar render, props: ");
+        console.log(this.prop);
+
+        return(
+            <div>
+                <header className="navbar App-header" id="nav-container">
+                    <div className="col-4">
+                        {loggedIn ? (
+                            <section className="navbar-section">
+                                <Link to="#" className="btn btn-link text-secondary" onClick={this.logout}>
+                                    <span className="text-secondary">Logout</span>
+                                </Link>
+                            </section>
+                        ) : (
+                            <section className="navbar-section">
+                                <Link to="/" className="btn btn-link text-secondary">
+                                    <span className="text-secondary">Home</span>
+                                </Link>
+                                <Link to="/login" className="btn btn-link text-secondary">
+                                    <span className="text-secondary">Sign Up</span>
+                                </Link>
+                            </section>
+                        )}
+                    </div>
+                    <div className="col-4 col-mr-auto">
+                        <div id="top-filler">
+                            <img src={logo} className="App-logo" alt="logo" />
+                            <h1 className="App-title">iEnrol</h1>
+                        </div>
+                    </div>
+                </header>
+            </div>
+        )
+    }
 }
+
+export default Navbar;
