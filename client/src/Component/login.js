8419298c5d4b776e0dbdd74a84e6f7ef1ed404bd
Polinet <uchpolinet@gmail.com>
2019-11-03 15:22:11 +1100

    render HTML form and input field for login component

--- client/src/Component/login.js
+++ client/src/Component/login.js
@@ -39,7 +39,50 @@ class Login extends Component {
                 })
                 this.setState({ redirectTo: "/"});
             }
+        }).catch(error => {
+            console.log("Login error occurr: ");
+            console.log(error);
         })
+    }
+
+    render() {
+        if (this.state.redirectTo) {
+            return <Redirect to={{ pathname: this.state.redirectTo }} />
+        }
+
+        else {
+            return (
+                <div>
+                    <h4>Login</h4>
+                    <form className="form-horizontal">
+                        <div className="form-group">
+                            <div className="col-1 col-ml-auto">
+                                <label className="form-label" htmlFor="username">Username</label>
+                            </div>
+
+                            <div className="col-3 col-mr-auto">
+                                <input className="form-input"
+                                    type="text"
+                                    id="username"
+                                    name="username"
+                                    placeholder="Username"
+                                    value={this.state.username}
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                        </div>
+                        
+                        <div className="form-group">
+                            <div className="col-7"></div>
+                            <button className="btn btn-primary col-1 col-mr-auto"
+                                onClick={this.handleSubmit}
+                                type="submit">Login</button>
+                        </div>
+                    </form>
+                </div>
+            )
+        }
     }
 }
 
+export default LoginForm
