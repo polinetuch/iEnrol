c602581e64da7337a705ecaa326116380815266a
Polinet <uchpolinet@gmail.com>
2019-11-03 14:26:58 +1100

    added validation if username is taken

--- client/src/Component/routes/user.js
+++ client/src/Component/routes/user.js
@@ -2,3 +2,22 @@ const express = require("express");
 const router = express.Router();
 const User = require("../database/user");
 const passport = require("../passport");
+
+router.post("/", (req, res) => {
+    console.log("User signup");
+
+    const { username, password} = req.body;
+
+    // Add validation
+    User.findOne({ username: username }, (err, user) => {
+        if (err) {
+            console.log("User.js post error: ", err);
+        } 
+        
+        else if (user) {
+            res.json({
+                error: `Sorry, username is unavailable: ${username}`
+            })
+        }
+    })
+})
