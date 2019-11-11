16f17444dea33015ba52bb2aecd5312ef831becb
Polinet <uchpolinet@gmail.com>
2019-11-11 21:38:32 +1100

    edited addEnrollment component using setState and create onChange function to hand update

--- client/src/components/newEnrollment/addEnrollment.js
+++ client/src/components/newEnrollment/addEnrollment.js
@@ -1,6 +1,32 @@
 import React, { Component } from 'react';
 
 class addEnrollment extends Component {
+    constructor(props) {
+        super(props)
+        this.onChangeStudentName = this.onChangeStudentName.bind(this);
+        this.onChangeAge = this.onChangeAge.bind(this);
+        this.onChangeGender = this.onChangeGender.bind(this);
+        this.onChangeMotherName = this.onChangeMotherName.bind(this);
+        this.onChangeFatherName = this.onChangeFatherName.bind(this);
+        this.onChangeContact = this.onChangeContact.bind(this);
+        this.onChangeAddress = this.onChangeAddress.bind(this);
+
+        this.state = {
+            name: "",
+            age: "",
+            gender: "",
+            mother: "",
+            father: "",
+            contact: "",
+            address: ""
+        }
+    }
+
+    onChangeStudentName(e) {
+        this.setState({
+            name: e.target.value
+        })
+    }
 
     render() {
         return(
