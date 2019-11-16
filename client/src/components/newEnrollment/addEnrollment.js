73d9e52d3ab5f259928cf5ebee3e369d8c73e3c6
Polinet <uchpolinet@gmail.com>
2019-11-16 14:56:03 +1100

    combine onChange value in editEnrollment into one function

--- client/src/components/newEnrollment/addEnrollment.js
+++ client/src/components/newEnrollment/addEnrollment.js
@@ -1,170 +1,170 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+// import React, { Component } from 'react';
+// import axios from 'axios';
 
-class AddEnrollment extends Component {
-    constructor(props) {
-        super(props)
-        this.onChangeStudentName = this.onChangeStudentName.bind(this);
-        this.onChangeAge = this.onChangeAge.bind(this);
-        this.onChangeGender = this.onChangeGender.bind(this);
-        this.onChangeMotherName = this.onChangeMotherName.bind(this);
-        this.onChangeFatherName = this.onChangeFatherName.bind(this);
-        this.onChangeContact = this.onChangeContact.bind(this);
-        this.onChangeAddress = this.onChangeAddress.bind(this);
+// class AddEnrollment extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.onChangeStudentName = this.onChangeStudentName.bind(this);
+//         this.onChangeAge = this.onChangeAge.bind(this);
+//         this.onChangeGender = this.onChangeGender.bind(this);
+//         this.onChangeMotherName = this.onChangeMotherName.bind(this);
+//         this.onChangeFatherName = this.onChangeFatherName.bind(this);
+//         this.onChangeContact = this.onChangeContact.bind(this);
+//         this.onChangeAddress = this.onChangeAddress.bind(this);
 
-        this.state = {
-            name: "",
-            age: "",
-            gender: "",
-            mother: "",
-            father: "",
-            contact: "",
-            address: "",
-            isAdmin: false
-        }
-    }
+//         this.state = {
+//             name: "",
+//             age: "",
+//             gender: "",
+//             mother: "",
+//             father: "",
+//             contact: "",
+//             address: "",
+//             isAdmin: false
+//         }
+//     }
 
-    onChangeStudentName(e) {
-        this.setState({
-            name: e.target.value
-        })
-    };
+//     onChangeStudentName(e) {
+//         this.setState({
+//             name: e.target.value
+//         })
+//     };
 
-    onChangeAge(e) {
-        this.setState({
-            age: e.target.value
-        })
-    };
+//     onChangeAge(e) {
+//         this.setState({
+//             age: e.target.value
+//         })
+//     };
 
-    onChangeGender(e) {
-        this.setState({
-            gender: e.target.value
-        })
-    };
+//     onChangeGender(e) {
+//         this.setState({
+//             gender: e.target.value
+//         })
+//     };
 
-    onChangeMotherName(e) {
-        this.setState({
-            mother: e.target.value
-        })
-    };
+//     onChangeMotherName(e) {
+//         this.setState({
+//             mother: e.target.value
+//         })
+//     };
 
-    onChangeFatherName(e) {
-        this.setState({
-            father: e.target.value
-        })
-    }
+//     onChangeFatherName(e) {
+//         this.setState({
+//             father: e.target.value
+//         })
+//     }
 
-    onChangeContact(e) {
-        this.setState({
-            contact: e.target.value
-        })
-    };
+//     onChangeContact(e) {
+//         this.setState({
+//             contact: e.target.value
+//         })
+//     };
 
-    onChangeAddress(e) {
-        this.setState({
-            address: e.target.value
-        })
-    };
+//     onChangeAddress(e) {
+//         this.setState({
+//             address: e.target.value
+//         })
+//     };
 
-    onSubmit(e) {
-        e.preventDefault();
-        const object = {
-            name: this.state.name,
-            age: this.state.age,
-            gender: this.state.gender,
-            mother: this.state.mother,
-            father: this.state.father,
-            contact: this.state.contact,
-            address: this.state.address
-        };
+//     onSubmit(e) {
+//         e.preventDefault();
+//         const object = {
+//             name: this.state.name,
+//             age: this.state.age,
+//             gender: this.state.gender,
+//             mother: this.state.mother,
+//             father: this.state.father,
+//             contact: this.state.contact,
+//             address: this.state.address
+//         };
 
-        axios.post("/enrollment/add", object).then((res) => {
-            console.log(82)
-            this.setState({
-                name: "",
-                age: "",
-                gender: "",
-                mother: "",
-                father: "",
-                contact: "",
-                address: ""
-            });
+//         axios.post("/enrollment/add", object).then((res) => {
+//             console.log(82)
+//             this.setState({
+//                 name: "",
+//                 age: "",
+//                 gender: "",
+//                 mother: "",
+//                 father: "",
+//                 contact: "",
+//                 address: ""
+//             });
         
-        }).then(() => {
-            console.log(94)
-            window.location = window.location.hostname
-        }).catch((err) => console.log(err))
-    }
+//         }).then(() => {
+//             console.log(94)
+//             window.location = window.location.hostname
+//         }).catch((err) => console.log(err))
+//     }
 
 
-    render() {
-        console.log('triggered Add En', this.props.isAdmin);
-        return(
-        <div>
-            <h3>Add New Enrollment</h3>
-                <form onSubmit={this.onSubmit}>
+//     render() {
+//         console.log('triggered Add En', this.props.isAdmin);
+//         return(
+//         <div>
+//             <h3>Add New Enrollment</h3>
+//                 <form onSubmit={this.onSubmit}>
 
-                    <div className="form-group">
-                        <label>Student Full Name </label>
-                        <input 
-                        type="text"
-                        className="form-control"
-                        value={this.state.name}
-                        onChange={this.onChangeStudentName} />
-                    </div>
+//                     <div className="form-group">
+//                         <label>Student Full Name </label>
+//                         <input 
+//                         type="text"
+//                         className="form-control"
+//                         value={this.state.name}
+//                         onChange={this.onChangeStudentName} />
+//                     </div>
 
-                    <div className="form-group">
-                        <label>Student Age </label>
-                        <input 
-                        type="text"
-                        className="form-control"
-                        value={this.state.age}
-                        onChange={this.onChangeAge} />
-                    </div>
+//                     <div className="form-group">
+//                         <label>Student Age </label>
+//                         <input 
+//                         type="text"
+//                         className="form-control"
+//                         value={this.state.age}
+//                         onChange={this.onChangeAge} />
+//                     </div>
 
-                    <div className="form-group">
-                        <label>Student Gender </label>
-                        <input 
-                        type="text"
-                        className="form-control"
-                        value={this.state.gender}
-                        onChange={this.onChangeGender} />
-                    </div>
+//                     <div className="form-group">
+//                         <label>Student Gender </label>
+//                         <input 
+//                         type="text"
+//                         className="form-control"
+//                         value={this.state.gender}
+//                         onChange={this.onChangeGender} />
+//                     </div>
 
-                    <div className="form-group">
-                        <label>Mother's Name </label>
-                        <input 
-                        type="text"
-                        className="form-control"
-                        value={this.state.mother}
-                        onChange={this.onChangeMother} />
-                    </div>
+//                     <div className="form-group">
+//                         <label>Mother's Name </label>
+//                         <input 
+//                         type="text"
+//                         className="form-control"
+//                         value={this.state.mother}
+//                         onChange={this.onChangeMother} />
+//                     </div>
 
-                    <div className="form-group">
-                        <label>Father's Name </label>
-                        <input 
-                        type="text"
-                        className="form-control"
-                        value={this.state.father}
-                        onChange={this.onChangeFather} />
-                    </div>
+//                     <div className="form-group">
+//                         <label>Father's Name </label>
+//                         <input 
+//                         type="text"
+//                         className="form-control"
+//                         value={this.state.father}
+//                         onChange={this.onChangeFather} />
+//                     </div>
 
-                    <div className="form-group">
-                        <label>Contact </label>
-                        <input 
-                        type="text"
-                        className="form-control"
-                        value={this.state.contact}
-                        onChange={this.onChangeContact} />
-                    </div>
-                    <div className="form-group">
-                        <input type="submit" value="Add New Student" className="btn btn-primary"/>
-                    </div>
-                </form>
-        </div>
+//                     <div className="form-group">
+//                         <label>Contact </label>
+//                         <input 
+//                         type="text"
+//                         className="form-control"
+//                         value={this.state.contact}
+//                         onChange={this.onChangeContact} />
+//                     </div>
+//                     <div className="form-group">
+//                         <input type="submit" value="Add New Student" className="btn btn-primary"/>
+//                     </div>
+//                 </form>
+//         </div>
 
-        )
-    }
-}
-;
-export default AddEnrollment;
+//         )
+//     }
+// }
+// ;
+// export default AddEnrollment;
--- client/src/components/newEnrollment/editEnrollment.js
+++ client/src/components/newEnrollment/editEnrollment.js
@@ -6,13 +6,7 @@ class EditEnrollment extends Component {
     constructor(props) {
         super(props);
         
-        this.onChangeStudentName = this.onChangeStudentName.bind(this);
-        this.onChangeAge = this.onChangeAge.bind(this);
-        this.onChangeGender = this.onChangeGender.bind(this);
-        this.onChangeMotherName = this.onChangeMotherName.bind(this);
-        this.onChangeFatherName = this.onChangeFatherName.bind(this);
-        this.onChangeContact = this.onChangeContact.bind(this);
-        this.onChangeAddress = this.onChangeAddress.bind(this);
+        this.onChangeData = this.onChangeData.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
         this.postData = this.postData.bind(this);
         this.updateData = this.updateData.bind(this);
@@ -38,6 +32,7 @@ class EditEnrollment extends Component {
             .then(res =>{
                 console.log(res);
                 this.setState({
+                    id: paramsID,
                     name: res.data.name,
                     age: res.data.age,
                     gender: res.data.gender,
@@ -45,7 +40,7 @@ class EditEnrollment extends Component {
                     father: res.data.father,
                     contact: res.data.contact,
                     address: res.data.address,
-                    id: paramsID
+                    uid: res.data.uid
                 })
             })
             .catch(function (error) {
@@ -55,53 +50,61 @@ class EditEnrollment extends Component {
         
     }
 
-    onChangeStudentName(e) {
-        this.setState({
-            name: e.target.value
-        })
-    };
+    // onChangeStudentName(e) {
+    //     this.setState({
+    //         name: e.target.value
+    //     })
+    // };
 
-    onChangeAge(e) {
-        this.setState({
-            age: e.target.value
-        })
-    };
+    // onChangeAge(e) {
+    //     this.setState({
+    //         age: e.target.value
+    //     })
+    // };
 
-    onChangeGender(e) {
-        this.setState({
-            gender: e.target.value
-        })
-    };
+    // onChangeGender(e) {
+    //     this.setState({
+    //         gender: e.target.value
+    //     })
+    // };
 
-    onChangeMotherName(e) {
-        this.setState({
-            mother: e.target.value
-        })
-    };
+    // onChangeMotherName(e) {
+    //     this.setState({
+    //         mother: e.target.value
+    //     })
+    // };
 
-    onChangeFatherName(e) {
-        this.setState({
-            father: e.target.value
-        })
-    };
+    // onChangeFatherName(e) {
+    //     this.setState({
+    //         father: e.target.value
+    //     })
+    // };
 
-    onChangeContact(e) {
-        this.setState({
-            contact: e.target.value
-        })
-    };
+    // onChangeContact(e) {
+    //     this.setState({
+    //         contact: e.target.value
+    //     })
+    // };
 
-    onChangeAddress(e) {
-        this.setState({
-            address: e.target.value
-        })
-    };
+    // onChangeAddress(e) {
+    //     this.setState({
+    //         address: e.target.value
+    //     })
+    // };
     
-    onChangeUid(e) {
+    // onChangeUid(e) {
+    //     this.setState({
+    //         uid: e.target.value
+    //     })
+    // };
+
+    onChangeData = e => {
+        const { name, value } = e.target;
+        console.log('triggered', name);
         this.setState({
-            uid: e.target.value
-        })
-    };
+            [name]: value
+        });
+    }
 
     updateData(data) {
         axios.post("/enrollment/update/" + this.state.id, data)
@@ -141,36 +144,36 @@ class EditEnrollment extends Component {
             <form onSubmit={this.onSubmit}>
                 <div className="form-group">
                     <label>Name</label>
-                    <input type="text" className="form-control" value={this.state.name} onChange={this.onChangeStudentName} />
+                    <input type="text" className="form-control" name="name" value={this.state.name} onChange={this.onChangeData} />
                 </div>
                 <div className="form-group">
                     <label>Age</label>
-                    <input type="text" className="form-control" value={this.state.age} onChange={this.onChangeAge} />
+                    <input type="text" className="form-control" name="age" value={this.state.age} onChange={this.onChangeData} />
                 </div>
                 <div className="form-group">
                     <label>Gender</label>
-                    <input type="text" className="form-control" value={this.state.gender} onChange={this.onChangeGender} />
+                    <input type="text" className="form-control" name="gender" value={this.state.gender} onChange={this.onChangeData} />
                 </div>
                 <div className="form-group">
                     <label>Mother</label>
-                    <input type="text" className="form-control" value={this.state.mother} onChange={this.onChangeMotherName} />
+                    <input type="text" className="form-control" name="mother" value={this.state.mother} onChange={this.onChangeData} />
                 </div>
                 <div className="form-group">
                     <label>Father</label>
-                    <input type="text" className="form-control" value={this.state.father} onChange={this.onChangeFatherName} />
+                    <input type="text" className="form-control" name="father" value={this.state.father} onChange={this.onChangeData} />
                 </div>
                 <div className="form-group">
                     <label>Contact</label>
-                    <input type="text" className="form-control" value={this.state.contact} onChange={this.onChangeContact} />
+                    <input type="text" className="form-control" name="contact" value={this.state.contact} onChange={this.onChangeData} />
                 </div>
                 <div className="form-group">
                     <label>Address</label>
-                    <input type="text" className="form-control" value={this.state.address} onChange={this.onChangeAddress} />
+                    <input type="text" className="form-control" name="address" value={this.state.address} onChange={this.onChangeData} />
                 </div>
-                {/* <div className="form-group">
+                <div className="form-group">
                     <label>User ID</label>
-                    <input type="text" className="form-control" value={this.state.uid} onChange={this.onChangeUid} />
-                </div> */}
+                    <input type="text" className="form-control" name="userId" value={this.state.uid} onChange={this.onChangeData} />
+                </div>
                 <div className="form-group">
                     <input type="submit" value={`${label} Enrollment`} className="btn btn-primary"/>
                 </div>
