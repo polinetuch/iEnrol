37bc1c328bfa73915216dc32369aea764818d95c
Polinet <uchpolinet@gmail.com>
2019-11-16 14:16:00 +1100

    redirect to home page after edit or delete

--- database/models/user.js
+++ database/models/user.js
@@ -7,8 +7,8 @@ mongoose.promise = Promise;
 const userSchema = new Schema({
 
 	username: { type: String, unique: false, required: false },
-	password: { type: String, unique: false, required: false }
-
+	password: { type: String, unique: false, required: false },
+	isAdmin: { type: Boolean, unique: false, required: false}
 });
 
 // Define schema methods
