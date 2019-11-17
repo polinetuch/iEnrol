966cea77838d6581bf62f4711ec6de5e1a5a8d67
Polinet <uchpolinet@gmail.com>
2019-11-17 17:35:41 +1100

    styled login and sign up form

--- client/src/components/sign-up.js
+++ client/src/components/sign-up.js
@@ -48,47 +48,43 @@ class Signup extends Component {
 
 	render() {
 		return (
-			<div className="SignupForm">
-				<h4>Sign up</h4>
-				<form className="form-horizontal">
-					<div className="form-group">
-						<div className="col-1 col-ml-auto">
-							<label className="form-label" htmlFor="username">Username</label>
-						</div>
-						<div className="col-3 col-mr-auto">
-							<input className="form-input"
-								type="text"
-								id="username"
-								name="username"
-								placeholder="Username"
-								value={this.state.username}
-								onChange={this.handleChange}
-							/>
-						</div>
-					</div>
-					<div className="form-group">
-						<div className="col-1 col-ml-auto">
-							<label className="form-label" htmlFor="password">Password: </label>
-						</div>
-						<div className="col-3 col-mr-auto">
-							<input className="form-input"
-								placeholder="password"
-								type="password"
-								name="password"
-								value={this.state.password}
-								onChange={this.handleChange}
-							/>
-						</div>
-					</div>
-					<div className="form-group ">
-						<div className="col-7"></div>
-						<button
-							className="btn btn-primary col-1 col-mr-auto"
-							onClick={this.handleSubmit}
-							type="submit"
-						>Sign up</button>
-					</div>
-				</form>
+			<div className="auth-wrapper">
+				<div className="auth-inner">
+			<form>
+                <h4>Sign Up</h4>
+
+                <div className="form-group">
+                    <label className="custom-control-label" htmlFor="username">Username</label>
+                    <input className="form-input"
+						type="text"
+						id="username"
+						name="username"
+						placeholder="Username"
+						value={this.state.username}
+						onChange={this.handleChange} 
+					/>
+                </div>
+
+                <div className="form-group">
+                    <label className="custom-control-label" htmlFor="password">Password</label>
+                    <input 
+						className="form-input"
+						placeholder="password"
+						type="password"
+						name="password"
+						value={this.state.password}
+						onChange={this.handleChange}
+					/>
+                </div>
+
+                <button className="btn btn-primary col-1 col-mr-auto"
+					onClick={this.handleSubmit}
+					type="submit">Sign Up</button>
+                {/* <p className="forgot-password text-right">
+                    Already a member? <a href="#">sign in?</a>
+                </p> */}
+            </form>
+			</div>
 			</div>
 		)
 	}
