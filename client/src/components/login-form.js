aedaf98a7226d9ca5681528b865df4ddf14a7e96
Polinet <uchpolinet@gmail.com>
2019-11-20 19:40:51 +1100

    styled table table - added border and a jumbotron in navbar

--- client/src/components/login-form.js
+++ client/src/components/login-form.js
@@ -57,7 +57,8 @@ class LoginForm extends Component {
         } else {
             return (
                 <>
-
+                <h2>Welcome to iEnrol</h2>
+                 <p>Have you ever been so busy and come to realize that your child’s enrollment form is due tomorrow? Do you ever need to step out of your office chair to try and call the admin so you can change your contact details and address? Or to find out about your loved one’s day being their first time at an educational setting? The anticipation is over! I have developed a solution that can save you time from taking annual leave or leave work early so you can have a chat with one of the admins at your child’s school or kinder.</p>
                 <div className="auth-wrapper">
                     <div className="auth-inner">
                 <form>
